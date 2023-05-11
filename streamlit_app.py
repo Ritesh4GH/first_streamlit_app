a65cc9b479cb65b7671d033e75881c566c2e6ead
Ritesh4GH <133235070+Ritesh4GH@users.noreply.github.com>
2023-05-11 22:08:17 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,5 +6,6 @@ streamlit.header('Breakfast Menu')
 streamlit.text('Omega 3 and Blueberry Oatmeal')
 streamlit.text('Kale Spinach and Rocket Smoothie')
 streamlit.text('Hard-Boiled Free-Range Egg')
-streamlit.text('Designed by Agrima and Anika')
+
+streamlit.title('Designed by Agrima and Anika')
                
